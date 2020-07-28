e35b0e5cc8e0bce9b581222eea7a4897346a9a1f
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-07-28 23:33:46 +0530

    Push FirstJava Maven Project

---
+++ Maven Connect/FirstJavaProject/src/main/java/com/wipro/project/App.java
@@ -0,0 +1,13 @@
+package com.wipro.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
