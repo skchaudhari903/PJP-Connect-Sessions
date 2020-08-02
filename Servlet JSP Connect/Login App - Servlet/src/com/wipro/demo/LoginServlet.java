eed3f3c69944ac2e7f28d5892a5d29f6ba783465
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-08-02 11:30:19 +0530

    Push LoginApp - Servlet

---
+++ Servlet JSP Connect/Login App - Servlet/src/com/wipro/demo/LoginServlet.java
@@ -0,0 +1,51 @@
+package com.wipro.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		String userName= request.getParameter("uname");
+		String password= request.getParameter("pwd");
+		if(userName.equals("Admin") && password.equals("wipro@123"))
+			out.println("<font color=green>welcome Admin</font>");
+		else
+			out.println("<font color=red>Invalid username or password</font>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	}
+
+}
