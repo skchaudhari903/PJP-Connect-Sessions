14fa77d0c5cbe1a04a00c0e7519e2388e802c8d4
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-08-05 16:47:19 +0530

    Push LoginApp - Servlet & JDBC

---
+++ Servlet JSP Connect/Login App - Servlet and JDBC/src/com/wipro/demo/LoginServlet.java
@@ -0,0 +1,66 @@
+package com.wipro.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		try{
+			PrintWriter out = response.getWriter();
+			String userName = request.getParameter("uname");
+			String password = request.getParameter("pwd");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
+			String sql = "select * from user_table where username=? and password=?";
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, userName);
+			st.setString(2, password);
+			ResultSet rs = st.executeQuery();
+			if(rs.next())
+				out.println("<font color=green><b>Welcome User!</b></font>");
+			else
+				out.println("<font color=red><b>Invalid user name or password</b></font>");
+			out.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
