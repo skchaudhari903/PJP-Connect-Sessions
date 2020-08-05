8d0b1f441ce52c8b225eeb90c0f2cc53242f78f2
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-08-05 19:50:28 +0530

    Push LoginApp - Servlet MVC

---
+++ Servlet JSP Connect/Login App - Servlet MVC/src/com/wipro/controller/LoginServlet.java
@@ -0,0 +1,62 @@
+package com.wipro.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wipro.bean.UserBean;
+import com.wipro.dao.UserDAO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("uname");
+		String password = request.getParameter("pwd");
+		UserBean userBean = new UserBean();
+		userBean.setUserName(userName);
+		userBean.setPassword(password);
+		UserDAO dao = new UserDAO();
+		String status = dao.checkUser(userBean);
+		if(status.equals("Success"))
+		{
+			response.sendRedirect("Success.html");
+		}
+		else{
+			response.sendRedirect("Fail.html");
+		}		
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Servlet JSP Connect/Login App - Servlet MVC/src/com/wipro/util/DBUtil.java
@@ -0,0 +1,19 @@
+package com.wipro.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}
