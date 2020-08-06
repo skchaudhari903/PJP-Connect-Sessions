8f00028d732eed46adb95f6a09e5e619a7a94c88
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-08-06 12:35:36 +0530

    Push RequestDispatcher - include

---
+++ Servlet JSP Connect/Inter Servlet Communication/RequestDispatcher - include/src/com/wipro/demo/WelcomeServlet2.java
@@ -0,0 +1,46 @@
+package com.wipro.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet2
+ */
+@WebServlet("/WelcomeServlet2")
+public class WelcomeServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String receivedName = request.getParameter("name");
+		PrintWriter out = response.getWriter();
+		out.print("<h2> Length of the name : " + receivedName.length() +"</h2>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
