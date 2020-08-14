55441e1357c89f1679dae51e9a5c03f262204887
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-08-14 15:21:28 +0530

    Push SpringIOCDemo-DI->Setter Injection

---
+++ Spring Core Connect/SpringIOCDemo/src/main/java/com/wipro/bean/Address.java
@@ -0,0 +1,47 @@
+package com.wipro.bean;
+
+public class Address {
+	private String street;
+	private String city;
+	private String pincode;
+	
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Address(String street, String city, String pincode) {
+		this.street = street;
+		this.city = city;
+		this.pincode = pincode;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", city=" + city + ", pincode=" + pincode + "]";
+	}
+
+}
